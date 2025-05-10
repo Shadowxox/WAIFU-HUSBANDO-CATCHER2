bd9f91aba3ad291f62c2014850ba10ef68b8b883
Shadowxox <shadowx0x12@gmail.com>
2025-05-10 16:09:58 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,20 +2,20 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7481211733"
-    sudo_users = "1496149228", "2022832277", "6925950092", "8134602296", "5877629357", "6861906489", "7152534468"
-    GROUP_ID = -1002337505439
+    OWNER_ID = "7795212861"
+    sudo_users = "7795212861", "5758240622", "7361967332"
+    GROUP_ID = -1002643948280
 
-    TOKEN = "7686081397:AAGe1wG2M_NsLHl_X5BLXzh88nv6Vmc6kZ0"
+    TOKEN = "7369319572:AAEqECI_jCRUYzXIPk2KmoUyXJs4vDYhA2w"
     mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
-    PARTNER = "7370569442", "6861852788", "7378476666"
+    PARTNER = "7361967332", "7795212861", "5758240622"
     PHOTO_URL = ["https://files.catbox.moe/7vr2im.jpg", "https://files.catbox.moe/7vr2im.jpg"]
-    SUPPORT_CHAT = "-1002337505439"
-    UPDATE_CHAT = "-1002337505439"
-    BOT_USERNAME = "@Capture_your_character_robot"
-    CHARA_CHANNEL_ID = "-1002337505439"
-    api_id = 26977255
-    api_hash = "f1d4b2272a2fd50ecdae431ca43fc045"
+    SUPPORT_CHAT = "https://t.me/hwkwjieie"
+    UPDATE_CHAT = "https://t.me/DBZ_COMMUNITY_2"
+    BOT_USERNAME = "@Dbz_waifubot"
+    CHARA_CHANNEL_ID = "-1002621413939"
+    api_id = 23287799
+    api_hash = "9f4f17dae2181ee22c275b9b40a3c907"
 
 
 class Production(Config):
