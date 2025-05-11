82fc9e3e11ce94a8d4029e295538028f3487b838
Shadowxox <shadowx0x12@gmail.com>
2025-05-11 10:46:08 +0530

    Update Harem.py

--- shivu/modules/Harem.py
+++ shivu/modules/Harem.py
@@ -27,7 +27,7 @@ RARITY_MAPPING = {
     '❄️ Winter Special': '❄️',
     '🌤️ Summer Special': '🌤',
     '🎴 AMV': '🎴',
-    '🎥 Hollywood: '🎥',
+    '🎥 Hollywood: '📽',
     # Add any additional rarities here
 }
 
