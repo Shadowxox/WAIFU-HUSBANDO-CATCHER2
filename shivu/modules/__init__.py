0ee803c63dc62e12542adc12565023a1a41f48c5
Shadowxox <shadowx0x12@gmail.com>
2025-05-14 20:35:30 +0530

    Delete shivu/modules/__init__.py

--- shivu/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ALL_MODULES = []
-
-# Dynamically import all .py files except __init__.py
-for file in os.listdir(os.path.dirname(__file__)):
-    if file.endswith(".py") and file != "__init__.py":
-        ALL_MODULES.append(file[:-3])
